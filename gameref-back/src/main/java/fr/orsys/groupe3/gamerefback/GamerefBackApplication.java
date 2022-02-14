c9d6a63daaa61386acc65113a02a27ad191ffe91
Kierann Corre <kierann.corre@gmail.com>
2022-02-14 10:28:51 +0100

    Créaction des projets back et front

---
+++ gameref-back/src/main/java/fr/orsys/groupe3/gamerefback/GamerefBackApplication.java
@@ -0,0 +1,13 @@
+package fr.orsys.groupe3.gamerefback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamerefBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamerefBackApplication.class, args);
+	}
+
+}
---
+++ gameref-back/src/test/java/fr/orsys/groupe3/gamerefback/GamerefBackApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.orsys.groupe3.gamerefback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamerefBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
