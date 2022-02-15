9062fb43f79f33ab2c75b1d6d700a6f0c1ad247f
PashwaMrooCoders <coderspashwamroo@gmail.com>
2022-02-15 16:59:27 +0100

    Maquette et Controller pour l'ajout et la suppression d'un jeu

--- gameref-back/src/main/java/fr/orsys/groupe3/gamerefback/controller/GameRestController.java
+++ gameref-back/src/main/java/fr/orsys/groupe3/gamerefback/controller/GameRestController.java
@@ -1,11 +1,38 @@
 package fr.orsys.groupe3.gamerefback.controller;
 
+import fr.orsys.groupe3.gamerefback.business.Game;
+import fr.orsys.groupe3.gamerefback.dao.GameDao;
+import fr.orsys.groupe3.gamerefback.dto.GameDto;
+import fr.orsys.groupe3.gamerefback.exception.NotFoundException;
+import fr.orsys.groupe3.gamerefback.service.GameService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @AllArgsConstructor
 @RestController
 @RequestMapping("/api")
 public class GameRestController {
+
+
+    private GameService gameService;
+
+
+    // Ajout d'un jeu
+    @PostMapping("/game")
+    public Game addGame(@RequestBody GameDto dto) throws NotFoundException {
+
+        return gameService.createGame(dto);
+    }
+
+    // Suppression d'un jeu
+    @DeleteMapping("game/{id}")
+    public Game deleteGame(@PathVariable("id") long id) throws NotFoundException {
+        return gameService.deleteGame(id);
+    }
+
+
+    @ExceptionHandler(NotFoundException.class)
+    public void handleNotFoundException(NotFoundException e) {
+        e.printStackTrace();
+    }
 }
