a349ae0b849752c0d119ee15069f71a0e54beb49
PashwaMrooCoders <coderspashwamroo@gmail.com>
2022-02-15 10:15:48 +0100

    Ajout du EER

--- gameref-back/src/main/java/fr/orsys/groupe3/gamerefback/controller/InitController.java
+++ gameref-back/src/main/java/fr/orsys/groupe3/gamerefback/controller/InitController.java
@@ -2,6 +2,14 @@ package fr.orsys.groupe3.gamerefback.controller;
 
 import org.springframework.stereotype.Controller;
 
+import javax.annotation.PostConstruct;
+
 @Controller
 public class InitController {
+
+
+    @PostConstruct
+    private void initAgeRating() {
+
+    }
 }
