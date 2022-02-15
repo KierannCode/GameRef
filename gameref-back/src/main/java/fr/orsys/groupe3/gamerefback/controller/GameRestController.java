5d85007323fd202c02e7b7fc3116f8306a53b49f
Kierann Corre <kierann.corre@gmail.com>
2022-02-15 16:46:10 +0100

    Implémenter le contrôlleur de modification d'un jeu

--- gameref-back/src/main/java/fr/orsys/groupe3/gamerefback/controller/GameRestController.java
+++ gameref-back/src/main/java/fr/orsys/groupe3/gamerefback/controller/GameRestController.java
@@ -1,11 +1,21 @@
 package fr.orsys.groupe3.gamerefback.controller;
 
+import fr.orsys.groupe3.gamerefback.business.Game;
+import fr.orsys.groupe3.gamerefback.dto.GameDto;
+import fr.orsys.groupe3.gamerefback.exception.NotFoundException;
+import fr.orsys.groupe3.gamerefback.service.GameService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-@AllArgsConstructor
 @RestController
 @RequestMapping("/api")
+@AllArgsConstructor
 public class GameRestController {
+    private GameService gameService;
+
+    @PatchMapping("/game/{id}")
+    public Game updateGame(@PathVariable Long id, @RequestBody GameDto dto) throws NotFoundException {
+        System.out.println("Hello from update");
+        return gameService.updateGame(id, dto);
+    }
 }
