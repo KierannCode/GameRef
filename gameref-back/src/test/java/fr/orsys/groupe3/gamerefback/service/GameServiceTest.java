d4ecbf014029ced7ce6436a5f12cb4bef1a8b815
jperaudon <crazy.jonjon@gmail.com>
2022-02-16 15:37:20 +0100

    Ajout des fichiers de classe de test pour le GameRestController et GameService

---
+++ gameref-back/src/test/java/fr/orsys/groupe3/gamerefback/service/GameServiceTest.java
@@ -0,0 +1,8 @@
+package fr.orsys.groupe3.gamerefback.service;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+public class GameServiceTest {
+}
